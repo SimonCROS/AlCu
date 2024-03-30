882a2856e41aea82aedec620e57fbfeee5dd4e86
SimonCROS <scros@student.42lyon.fr>
2024-03-30 15:03:39 +0100

    Add macros

--- includes/alcu.h
+++ includes/alcu.h
@@ -5,6 +5,10 @@
 #include "ft_vector.h"
 #include "ft_mem.h"
 
+#define SET_SHOULD_WIN_FLAG(num, condition) num |= (condition) << 31
+#define SHOULD_WIN(num) num & (1 << 31)
+#define N(num) num & ~(1 << 31)
+
 #define EMPTY 0
 #define ERROR -1
 #define END_OF_FILE -2
